7d7a776acc3c4561b9e2c1b03ec18bdde5221b84
prasoonchatterjee <50299354+prasoonchatterjee@users.noreply.github.com>
2021-01-28 16:44:22 +0530

    header added

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,11 +3,32 @@ import {
   JumbotronContainer,
   FooterContainer,
   FaqsContainer,
+  HeaderContainer,
 } from "../containers";
 
+import { OptForm, Feature } from "../components";
 function home() {
   return (
     <>
+      <HeaderContainer>
+        <Feature>
+          <Feature.Title>
+            Unlimited films, TV programmes and more.
+          </Feature.Title>
+          <Feature.SubTitle>
+            Watch anywhere. Cancel at any time.
+          </Feature.SubTitle>
+          <OptForm>
+            <OptForm.Input placeholder="Email Address" />
+            <OptForm.Button>Try it now</OptForm.Button>
+            <OptForm.Break />
+            <OptForm.Text>
+              Ready to watch? Enter your email to create or restart your
+              membership
+            </OptForm.Text>
+          </OptForm>
+        </Feature>
+      </HeaderContainer>
       <JumbotronContainer />
       <FaqsContainer />
       <FooterContainer />
