be974ea05a82433b126ce26202312febfc83f9ae
prasoonchatterjee <50299354+prasoonchatterjee@users.noreply.github.com>
2021-02-05 10:09:30 +0530

    utils for browse page

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,10 +1,14 @@
 import React from "react";
 
 import { useContent } from "../hooks";
+import { selectionFilter } from "../utils";
 
 function Browse() {
   const { films } = useContent("films");
   const { series } = useContent("series");
+  const slides = selectionFilter({ series, films });
+  console.log(slides);
+
   return <p>hello from Browse</p>;
 }
 
