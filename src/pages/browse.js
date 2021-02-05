6bc3ec8b81c37cbcb43538f82357324d2283c966
prasoonchatterjee <50299354+prasoonchatterjee@users.noreply.github.com>
2021-02-05 09:58:33 +0530

    useContent custom hook added

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,6 +1,10 @@
 import React from "react";
 
+import { useContent } from "../hooks";
+
 function Browse() {
+  const { films } = useContent("films");
+  const { series } = useContent("series");
   return <p>hello from Browse</p>;
 }
 
